f56e46a0e8765c3956febdc3b780e25e14f93fed
idheeraj2603 <dheerajy2603@gmail.com>
2025-11-10 13:17:14 +0530

    Done

--- Wrapper/Q9_PrimitiveInGenerics.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Q9_PrimitiveInGenerics {
-    public static void main(String[] args) {
-        // ArrayList<int> list = new ArrayList<int>(); // not allowed
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        System.out.println(list);
-    }
-}
