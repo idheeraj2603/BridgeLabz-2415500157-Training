9b84492a112642e7c9c37c7bcb26e797683c2544
idheeraj2603 <dheerajy2603@gmail.com>
2025-11-14 17:29:59 +0530

    Dheeraj added file

--- Wrapper/Q2_DoubleToString.java
+++
@@ -1,7 +0,0 @@
-public class Q2_DoubleToString {
-    public static void main(String[] args) {
-        double num = 25.67;
-        String str = Double.toString(num);
-        System.out.println(str);
-    }
-}
--- Wrapper/Q4_StringToDoubleIntValue.java
+++
@@ -1,7 +0,0 @@
-public class Q4_StringToDoubleIntValue {
-    public static void main(String[] args) {
-        String str = "45.67";
-        Double d = Double.valueOf(str);
-        System.out.println(d.intValue());
-    }
-}
